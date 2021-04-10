1c639254601eef728b1e45c8f08c2fab5e59350f
Shmaither M. Rizo <shmaither@gmail.com>
2021-04-10 03:27:05 +0000

    Add token authorization and access to favorites table

--- src/main.py
+++ src/main.py
@@ -9,9 +9,13 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Character, Planet
-# from flask_jwt_extended import JWTManager
-# from flask_jwt_extended import create_access_token
-#from models import Person
+from service import Service
+
+# import Flask-JWT-Extended library
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -22,8 +26,8 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 # Setup the Flask-JWT-Extended extension
-# app.config["JWT_SECRET_KEY"] = os.environ.get('JW_TOKEN')  # Change this "super secret" with something else!
-# jwt = JWTManager(app)
+app.config["JWT_SECRET_KEY"] = os.environ.get('JW_TOKEN')  # Change this "super secret" with something else!
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -34,6 +38,29 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Query your database for username and password
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({"token": access_token})
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"email": user.email, "password": user.password }), 200
 
 # Users endpoints
 @app.route('/users', methods=['GET'])
@@ -90,13 +117,47 @@ def delete_planet(position):
     print("This is the position to delete: ",position)
     temp = Planet.deletePlanet(position)
     response_body = {
-        "msg": "Character deleted "
+        "msg": "Planet deleted "
     }
 
     return jsonify(response_body), 200
 
+# Favorite endpoints:
+# @app.route('/favorite', methods=['GET'])
+# @jwt_required()
+# def get_all_favorite():
+    
+#     # Access the identity of the current user with get_jwt_identity
+#     current_user_id = get_jwt_identity()
+
+#     all_favorites = Service.get_favorites(current_user_id)
+#     return jsonify(all_favorites), 200
+
+# @app.route('/favorite', methods=['POST'])
+# def add_favorite():
+#     request_body = request.get_json()
+#     favorite = Favorite(item_id=request_body["item_id"], item_type=request_body["item_type"], user_id=request_body["user_id"])
+#     db.session.add(favorite)
+#     db.session.commit()
+#     print("Favorite added: ", request_body)
+#     return jsonify(request_body), 200
+
+# @app.route('/favorite/<int:id>', methods=['DELETE'])
+# def delete_favorite(id):
+#     favorite = Favorite.query.get(id)
+
+#     if favorite is None:
+#         raise APIException('Favorite not found', status_code=404)
+
+#     db.session.delete(favorite)
+#     db.session.commit()
+#     response_body = {
+#          "msg": "Favorite delete successful",
+#     }
+#     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
---
+++ src/service.py
@@ -0,0 +1,40 @@
+from models import db, User, Character, Planet, Favorite
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    
+
+class Service:
+   
+    def get_favorite_per_type(fav):
+        # print(bcolors.WARNING + str(fav) + bcolors.ENDC)
+        if fav.item_type == "planet":
+            planet = Planet.query.get(fav.item_id)
+            return planet.serialize()
+        if fav.item_type == "character":
+            character = Character.query.get(fav.item_id)
+            return character.serialize()
+            
+        return None
+
+    def get_favorites(user_id):
+        
+        user = User.query.get(user_id)
+        if user is None:
+            raise APIException('User not found', status_code=404)
+
+        all_favorites = Favorite.query.all()
+
+        all_favorites = list(map(lambda x: get_favorite_per_type(x), all_favorites)) 
+
+        return all_favorites
+
